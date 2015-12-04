9f0961c38d39275d0f0ca2ad258c7be3b0629bfd
Ariel Dexler <bad225@nyu.edu>
2015-12-04 03:19:59 -0500

    Rewrote all modules except Game. Need to test code.

--- Logic.js
+++ Logic.js
@@ -43,10 +43,10 @@ Player = function (playerId, pieces, playerName) {
         //For all inplay pieces move them
         this._inPlay.forEach(function (piece) {
             piece.advance()
-        })
+        });
         //If any of the pieces become dead then remove from the inplay
         clearDead();
-    }
+    };
 
     /**
      * Used to make the initial move for a an unused piece, requires the node and the program for the piece
@@ -62,24 +62,24 @@ Player = function (playerId, pieces, playerName) {
         } else {
             return false;
         }
-    }
+    };
 
     this.getPlayerName = function () {
         return this._playerName;
-    }
+    };
 
     this.setNextMove = function (nextMove) {
         this._nextMove = nextMove;
-    }
+    };
 
     this.getNextMove = function () {
         return this._nextMove;
-    }
+    };
 
     this.toString = function () {
         clearDead();
         return null;
-    }
+    };
 
     var clearDead = function () {
         var i = this._inPlay.length;
@@ -122,16 +122,11 @@ Player = function (playerId, pieces, playerName) {
 
 var  Piece = function(playerId) {
     this._playerId=playerid;
-    this._location;
+    this._location=null;
     this._program=[];
-    this._moveCount;
-    this._isDead;
-    this._nodesEaten;
-
-
-
-
-
+    this._moveCount=0;
+    this._isDead=false;
+    this._nodesEaten=0;
 
     var programPiece=function(moveOrder){
         if(isValidProgram(moveOrder)){
@@ -150,67 +145,316 @@ var  Piece = function(playerId) {
         }
 
         //Should have each move type
-        return moveOrder.contains(Direction.UP) && moveOrder.contains(Direction.DOWN) &&
-                moveOrder.contains(Direction.LEFT) && moveOrder.contains(Direction.RIGHT);
+        return moveOrder.indexof(Direction.UP)>-1 && moveOrder.inedxOf(Direction.DOWN)>-1 &&
+                moveOrder.indexOf(Direction.LEFT)>-1 && moveOrder.indexOf(Direction.RIGHT)>-1;
     }
 
-    public Direction advance(){
-        if(isDead == false) {
-            final int numberOfDirection = 4;
-
-            if (location != null && isValidProgram(program)) {
+    this.advance=function(){
+        if(this._isDead == false) {
+            var numberOfDirection = 4;
+            Object.freeze(numberOfDirection) ;
+            if (this._location != null && isValidProgram(this._program)) {
                 //Search for valid move
-                for (int i = moveCount; i < moveCount + numberOfDirection; i++) {
-                    Direction nextMove = program.get((i) % numberOfDirection);
+                for (i = this._moveCount; i < this._moveCount + numberOfDirection; i++) {
+                    var nextMove = this._program.get((i) % numberOfDirection);
 
-                    if (location.canMove(nextMove)) {
-                        location.move(nextMove);
-                        moveCount = i + 1;
+                    if (this._location.canMove(nextMove)) {
+                        this._location.move(nextMove);
+                        this._moveCount = i + 1;
                         return nextMove;
                     }
                 }
 
                 //If no valid move available then the piece is dead
-                isDead = true;
+                this._isDead = true;
                 return null;
             }
         }
         return null;
     }
 
-    public void placeOnNode(Node node){
-        this.location = node;
-        nodesEaten++;
+    this.placeOnNode=function( node){
+        this._location = node;
+        this._nodesEaten++;
     }
 
-    public PlayerId getPlayerId(){
-        return this.playerId;
+    this.getPlayerId=function(){
+        return this._playerId;
     }
 
-    public int getNodesEaten(){
-        return this.nodesEaten;
+    this.getNodesEaten=function(){
+        return this._nodesEaten;
     }
 
-    public boolean isDead(){
-        return this.isDead;
+    this.isDead=function(){
+        return this._isDead;
     }
 
-    public Direction getPreviousDirection(){
-        if(moveCount > 0 ){
-            return program.get((moveCount - 1) % 4);
+    this.getPreviousDirection=function(){
+        if(this._moveCount > 0 ){
+            return this._program.get((this._moveCount - 1) % 4);
         }else{
             return null;
         }
 
     }
 
-    public void killPiece(){
-        this.isDead = true;
+    this.killPiece=function(){
+        this._isDead = true;
     }
 
-    public boolean isNewPiece(){
+    this.isNewPiece=function(){
         //System.out.println(moveCount);
-        return moveCount == 0;
+        return this.__moveCount == 0;
     }
 
 }
+
+var  Node=function(id, xLoc,  yLoc) {
+    this.id=id;
+    this.id = id;
+    this.xLoc = xLoc;
+    this.yLoc = yLoc;
+    this._status = Status.FREE;
+   // @JsonIgnore
+    this.up;
+   // @JsonIgnore
+    this.down;
+    //@JsonIgnore
+    this.left;
+   // @JsonIgnore
+    this.right;
+
+    this._occupant;
+    this._desireOccupancy = [];
+
+
+
+
+
+    this.getId=function(){
+        return this.id;
+    }
+
+    this.toString=function(){
+        var up = nullCheck(this.up);
+        var  down = nullCheck(this.down);
+        var left = nullCheck(this.left);
+        var right= nullCheck(this.right);
+
+        return this.id + "," + this.xLoc + "," + this.yLoc + "," + this._status + "," + this.up + "," + this.down + "," + this.left + "," + this.right;
+    }
+
+    this.nullCheck=function(node){
+        if(node == null){
+            return "null";
+        }else {
+            return node.getId();
+        }
+    }
+
+    this.canMove=function(direction){
+
+        switch (direction){
+            case Direction.UP:
+                if(up == null){
+                    return false;
+                }else{
+                    return up.isFree();
+                }
+            case Direction.DOWN:
+                if(down == null){
+                    return false;
+                }else{
+                    return down.isFree();
+                }
+            case Direction.LEFT:
+                if(left == null){
+                    return false;
+                }else{
+                    return left.isFree();
+                }
+            case Direction.RIGHT:
+                if(right == null){
+                    return false;
+                }else{
+                    return right.isFree();
+                }
+            default:return false;
+        }
+    }
+
+    this.connect=function(node, direction){
+        switch (direction){
+            case Direction.UP:
+                up = node;
+            case Direction.DOWN:
+                down = node;
+            case Direction.LEFT:
+                left = node;
+            case Direction.RIGHT:
+                right = node;
+        }
+    }
+
+    this.isFree=function(){
+        return this._status.equals(Status.FREE);
+    }
+
+    this.move=function(direction){
+            switch (direction){
+                case Direction.UP:
+                    up.place(this._occupant);
+                    this._occupantplaceOnNode(up);
+                    break;
+                case Direction.DOWN:
+                    down.place(this._occupant);
+                    this._occupant.placeOnNode(down);
+                    break;
+                case Direction.LEFT:
+                    left.place(this._occupant);
+                    this._occupant.placeOnNode(left);
+                    break;
+                case Direction.RIGHT:
+                    right.place(this._occupant);
+                    this._occupant.placeOnNode(right);
+                    break;
+            }
+    }
+
+    this.place=function(piece){
+        this._desireOccupancy.add(piece);
+    }
+
+    /**
+     * After all pieces have moved onto thier desired node decide which ones to keep and which ones to eject
+     */
+    this.moveTime=function(playerWithPriority){
+        //First check if the node is currently occupied. If so then move the occupant along
+        if(this._occupant != null){
+            if(this._occupant.getPlayerId()==(PlayerId.ONE)){
+                this._status = Status.EATEN_P1;
+            }else{
+                this._status = Status.EATEN_P2;
+            }
+        }
+
+        var occuopantSelected = false;
+        var newPieces = [];
+
+        this._desireOccupancy.map(function(piece){
+
+            if(piece.getPreviousDirection() != null){
+                //First order is piece that moved up to get here
+                if(piece.getPreviousDirection()==(Direction.UP)){
+                    occupy(piece);
+                    occuopantSelected = true;
+                }
+
+                //Second is piece that moved left
+                if(piece.getPreviousDirection()==(Direction.LEFT)){
+                    if(occuopantSelected){
+                        piece.killPiece();
+                    }else{
+                        occupy(piece);
+                        occuopantSelected = true;
+                    }
+                }
+
+                //Third is a piece that moved down
+                if(piece.getPreviousDirection()==(Direction.DOWN)){
+                    if(occuopantSelected){
+                        piece.killPiece();
+                    }else{
+                        occupy(piece);
+                        occuopantSelected = true;
+                    }
+                }
+
+                //Fourth is a piece that moved right
+                if(piece.getPreviousDirection()==(Direction.RIGHT)){
+                    if(occuopantSelected){
+                        piece.killPiece();
+                    }else{
+                        occupy(piece);
+                        occuopantSelected = true;
+                    }
+                }
+            }
+
+            //Fifth is a new piece
+            if(piece.isNewPiece()){
+                newPieces.push(piece);
+            }
+        }
+        );
+        newPieces.map(function(piece){
+            if(occuopantSelected ||(newPieces.length > 1 && piece.getPlayerId() != playerWithPriority)){
+                piece.killPiece();
+            }else{
+                occupy(piece);
+                occuopantSelected = true;
+            }
+        }  )
+
+        this._desireOccupancy.clear();
+
+    }
+
+   this.getxLoc=function(){
+        return this.xLoc;
+    }
+
+   this.getyLoc=function(){
+        return this.yLoc;
+    }
+
+    /**
+     * Used to connect two nodes. Calculates if the given node is up, down, left, or right of the current node.
+     * Returns the direction.
+     * @param node
+     * @return
+     */
+    this.connect=function(node){
+
+        //If same x then node is above or below
+        if(node.getxLoc() == this.xLoc){
+            if(node.getyLoc() > this.yLoc){
+                this.up = node;
+              //  System.out.println("UP: current:" + this.yLoc + " above: " + node.getyLoc());
+                return Direction.UP;
+            }else{
+                this.down = node;
+           //     System.out.println("DOWN: current:" + this.yLoc + " below: " + node.getyLoc());
+                return Direction.DOWN;
+
+            }
+        }else{
+            //If same Y then node is left or right
+            if(node.getxLoc() > this.xLoc){
+                this.right = node;
+             //   System.out.println("Right: current:" + this.xLoc + " right: " + node.getxLoc());
+                return Direction.RIGHT;
+            }else{
+                this.left = node;
+              //  System.out.println("Left: current:" + this.xLoc + " left: " + node.getxLoc());
+                return Direction.LEFT;
+            }
+        }
+    }
+
+    var occupy=function(piece){
+        this._occupant = piece;
+        if(piece.getPlayerId() == PlayerId.ONE){
+            this._status = Status.OCCUPIED_P1;
+        }else{
+            this._status = Status.OCCUPIED_P2;
+        }
+    };
+
+    this.getStatus=function(){
+        return this._status;
+    }
+
+
+};
