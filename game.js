3b5abf7128390ce8f8c8029cf231607cab59563d
Islam Tawfik <ibtawfik@gmail.com>
2015-12-12 23:59:37 -0500

    Added controls to add player program.

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ angular.module('myApp')
         function ($scope, Logic) {
                 var board = Logic.getBoard();
                 var player = Logic.currentPlayer();
+                var stored;
                 $scope.isVoid = function(row,column){
                         return board[row][column].status === "void";
                 };
@@ -53,13 +54,15 @@ angular.module('myApp')
                         }
 
                 };
-                $scope.update=function(row,column){
+
+                function update (row,column,directions){
+                        stored = null;
                         if(Logic.isValidMove(row,column,player)){
-                                Logic.makeMove(row,column,player,["UP","DOWN","LEFT","RIGHT"]);
+                                Logic.makeMove(row,column,player,format(direction));
                         }
                         console.log(row + " " + column);
                         console.log(Logic.getBoard()[row][column])
-                };
+                }
 
                 $scope.setGameMode=function(gameMode){
                         Logic.setGameMode(gameMode);
@@ -68,6 +71,71 @@ angular.module('myApp')
                 $scope.completeTurn=function(){
                         console.log("TURN COMPLETE");
                         Logic.completeTurn();
+                };
+
+                $scope.store = function(row,column){
+                        stored = {r:row,c:column};
+                };
+
+                $scope.showDirectionInput = function(){
+                     if(stored){
+                             return true;
+                     }else{
+                             return false;
+                     }
+                };
+
+                $scope.getLeft=function(){
+                        return 10;
+                };
+
+                $scope.getTop=function(){
+                        return 50;
+                };
+
+                $scope.test = function(element){
+                        if(element.keyCode == 13){
+                                var elem = element.srcElement || element.target;
+                               validateProgram(elem.value);
+                        }
+                };
+
+                function validateProgram(value){
+                        value = value.toUpperCase();
+                        var chars = value.split('');
+
+                        if(isValidInputString(chars)){
+                                update(stored.r, stored.c,chars)
+                        }
+
+                }
+
+                function isValidInputString(value){
+                        //Check the length of the string
+                        if(value.length !== 4){
+                                return false;
+                        }
+
+                        //Check that it has one of each type
+                        return value.includes("U") && value.includes("D") &&
+                            value.includes("L") && value.includes("R");
+                }
+
+                function format(direction){
+                        var returnVals = [];
+                        for(var i = 0; i < direction.length; i++){
+                                var val = direction[i];
+                                if(val === "U"){
+                                        returnVals.push("UP");
+                                }else if(val === "D"){
+                                        returnVals.push("DOWN");
+                                }else if(val === "R"){
+                                        returnVals.push("RIGHT");
+                                }else if(val === "L"){
+                                        returnVals.push("LEFT");
+                                }
+                        }
+                        return returnVals;
                 }
 
             }]);
